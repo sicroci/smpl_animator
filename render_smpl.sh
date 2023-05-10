045ce0ef4c184539bfac78ff31a7413b158c8bbe
Simone Croci <simone.croci@gixel.de>
2023-05-10 16:38:03 +0200

    Adding bash script for the rendering of the SMPL model

---
+++ render_smpl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Generation of yaml files
+python2 code/runs/runs_debug.py --run run_debug_01
+
+# Running animation
+python2 code/cli/cli_gen_smpl_anim_frames.py --run /body_reconstruction/smpl_animator/data/runs/run_debug_01.yaml
